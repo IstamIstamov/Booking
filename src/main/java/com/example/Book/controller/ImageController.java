439fcf14ed8e31ff8fc35771f66c5d7e3c8eb4c0
IstamIstamov <istamovistam34@gmail.com>
2023-03-30 13:30:12 +0500

    add image

--- src/main/java/com/example/Book/controller/ImageController.java
+++ src/main/java/com/example/Book/controller/ImageController.java
@@ -1,4 +1,36 @@
 package com.example.Book.controller;
 
+import com.example.Book.dto.ImageDto;
+import com.example.Book.dto.ResponseDto;
+import com.example.Book.service.ImageService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("image")
 public class ImageController {
+    private ImageService imageService;
+
+    private ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/create")
+    public ResponseDto<ImageDto> createImage(@RequestBody ImageDto imageDto) {
+        return imageService.create(imageDto);
+    }
+
+    @GetMapping("/get")
+    public ResponseDto<ImageDto> getImage(@RequestParam Integer id) {
+        return imageService.get(id);
+    }
+
+    @PostMapping("/update")
+    public ResponseDto<ImageDto> updateImage(@RequestBody ImageDto imageDto, @RequestParam Integer id) {
+        return imageService.update(imageDto, id);
+    }
+
+    @PostMapping("/delete")
+    public ResponseDto<ImageDto> deleteImage(@RequestParam Integer id) {
+        return imageService.delete(id);
+    }
 }
--- src/main/java/com/example/Book/dto/ImageDto.java
+++ src/main/java/com/example/Book/dto/ImageDto.java
@@ -1,4 +1,18 @@
 package com.example.Book.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ImageDto {
+    private Integer imageId;
+    private String path;
+    private String type;
+    private Long size;
+    private String token;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
 }
